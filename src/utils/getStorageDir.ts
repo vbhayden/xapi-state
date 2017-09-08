e72edd8db14282e1d2f8da411c57ba6f414d3779
James Mullaney <james@ht2.co.uk>
2017-09-08 16:40:31 +0000

    Join storage dir

--- src/utils/getStorageDir.ts
+++ src/utils/getStorageDir.ts
@@ -1,6 +1,8 @@
+import { join } from 'path';
+
 export interface GetStorageDirOptions {
   readonly subfolder: string;
   readonly lrs_id: string;
 }
 
-export default (opts: GetStorageDirOptions) => `${opts.subfolder}/${opts.lrs_id}/states`;
+export default (opts: GetStorageDirOptions) => join(opts.subfolder, opts.lrs_id, 'states');
